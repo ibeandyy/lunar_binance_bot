5a4b20a2de728a4b957a91790aa0a86b77a79c65
ibeandyy <adhyr4@gmail.com>
2021-09-15 16:38:59 -0700

    further obfuscation and comments

--- main.py
+++ main.py
@@ -3,91 +3,92 @@ from binance import Client
 import time
 import json
 from LicenseManager import license_check
-#DEBUG LICENSE TO REMOVE
-license_key = '5nb4-4w47-p0aa-zudq-56sq'
-
-#license_key = input("Please enter license key:")
-while license_check(license_key):
-
-    # Grab API Data, Token pair to Trade, Amount to buy from local JSON
-    with open('config.json') as f:
-        data = json.load(f)
-
-    apiKey = data['apiKey']
-    apiSecret = data['apiSecret']
-    ####TestNet API Keys OVERRIDE###################################################
-    apiKey = 'FWSZColzUi6onFbxDkm4XWNmVcG46vhh7h1QcL7Zz26pWyjHDaK7rx2CQByM6y5X'   #
-    apiSecret = 'Mw9Bp2hDTWkqMfa3cNrArDRMB0bmbPYG43IOSwEjjm0w8JXRv8MteOzUHADo7FLS'#
-    ###############################################################################
-    symbol = data['tokenSymbol']
-    buyAmount = data['buyAmount']
-    sellAmount = data['sellAmount']
-
-    # Connect to API using config.json
-    # Remove testnet=True to interact with mainnet
-    # Remove tld='us' if using binance.com rather than binance.us
-    client = Client(apiKey, apiSecret, tld='us', testnet=True)
 
 
-    def moon_phase(month, day, year):
-        ages = [18, 0, 11, 22, 3, 14, 25, 6, 17, 28, 9, 20, 1, 12, 23, 4, 15, 26, 7]
-        offsets = [-1, 1, 0, 1, 2, 3, 4, 5, 7, 7, 9, 9]
-        description = ["new moon",
-                       "waxing crescent ",
-                       "1/4 moon",
-                       "waxing gibbous ",
-                       "full moon",
-                       "waning gibbous (",
-                       "3/4 moon",
-                       "waning crescent "]
-        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-                  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+if __name__ == '__main__':
 
-        if day == 31:
-            day = 1
-        days_into_phase = ((ages[(year + 1) % 19] +
-                            ((day + offsets[month - 1]) % 30) +
-                            (year < 1900)) % 30)
-        index = int((days_into_phase + 2) * 16 / 59.0)
-        # print(index)  # test
-        if index > 7:
-            index = 7
-        status = description[index]
+    license_key = input("Please enter license key:")
+    while license_check(license_key):
 
-        # light should be 100% 15 days into phase
-        light = int(2 * days_into_phase * 100 / 29)
-        if light > 100:
-            light = abs(light - 200);
-        date = "%d %s %d" % (day, months[month - 1], year)
+        # Grab API Data, Token pair to Trade, Amount to buy from local JSON
+        with open('config.json') as f:
+            data = json.load(f)
 
-        return status, light  # date, status, light
+        apiKey = data['apiKey']
+        apiSecret = data['apiSecret']
+        ####TestNet API Keys OVERRIDE###################################################
+        #apiKey = 'FWSZColzUi6onFbxDkm4XWNmVcG46vhh7h1QcL7Zz26pWyjHDaK7rx2CQByM6y5X'   #
+        #apiSecret = 'Mw9Bp2hDTWkqMfa3cNrArDRMB0bmbPYG43IOSwEjjm0w8JXRv8MteOzUHADo7FLS'#
+        ###############################################################################
+        symbol = data['tokenSymbol']
+        buyAmount = data['buyAmount']
+        sellAmount = data['sellAmount']
+
+        # Connect to API using config.json
+        # Remove testnet=True to interact with mainnet
+        # Remove tld='us' if using binance.com rather than binance.us
+        client = Client(apiKey, apiSecret, tld='us', testnet=True)
 
 
-    def moon_check(todays_moon, light):
-        # Returns True if moon is in final 3 days of waxing gibbous indicating a buy
-        if "waxing gibbous" in todays_moon and light >= 90:
-            return True
-        # Returns False if moon is in final 3 days of waning crescent indicating a sell
-        elif "waning crescent" in todays_moon and light <= 19:
-            sell_flag = True
-            return False
-        # Return None if neither is satisfied, indicating no action is to be taken.
-        else:
-            return None
+        def moon_phase(month, day, year):
+            ages = [18, 0, 11, 22, 3, 14, 25, 6, 17, 28, 9, 20, 1, 12, 23, 4, 15, 26, 7]
+            offsets = [-1, 1, 0, 1, 2, 3, 4, 5, 7, 7, 9, 9]
+            description = ["new moon",
+                           "waxing crescent ",
+                           "1/4 moon",
+                           "waxing gibbous ",
+                           "full moon",
+                           "waning gibbous (",
+                           "3/4 moon",
+                           "waning crescent "]
+            months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+            if day == 31:
+                day = 1
+            days_into_phase = ((ages[(year + 1) % 19] +
+                                ((day + offsets[month - 1]) % 30) +
+                                (year < 1900)) % 30)
+            index = int((days_into_phase + 2) * 16 / 59.0)
+            # print(index)  # test
+            if index > 7:
+                index = 7
+            status = description[index]
+
+            # light should be 100% 15 days into phase
+            light = int(2 * days_into_phase * 100 / 29)
+            if light > 100:
+                light = abs(light - 200);
+            date = "%d %s %d" % (day, months[month - 1], year)
+
+            return status, light  # date, status, light
 
 
-    # If one of the above is true, buy/sell
+        def moon_check(todays_moon, light):
+            # Returns True if moon is in final 3 days of waxing gibbous indicating a buy
+            if "waxing gibbous" in todays_moon and light >= 90:
+                return True
+            # Returns False if moon is in final 3 days of waning crescent indicating a sell
+            elif "waning crescent" in todays_moon and light <= 19:
+                sell_flag = True
+                return False
+            # Return None if neither is satisfied, indicating no action is to be taken.
+            else:
+                return None
 
 
-    def buy_order(token_pair, amount):
-        client.order_market_sell(symbol=token_pair, quantity=amount)
+        # If one of the above is true, buy/sell
 
 
-    def sell_order(token_pair, amount):
-        client.order_market_buy(symbol=token_pair, quantity=amount)
+        def buy_order(token_pair, amount):
+            client.order_market_sell(symbol=token_pair, quantity=amount)
 
 
-    if __name__ == '__main__':
+        def sell_order(token_pair, amount):
+            client.order_market_buy(symbol=token_pair, quantity=amount)
+
+
+
         while True:
             # Get today's date,
             today = str(date.today().strftime('%m,%d,%Y'))
@@ -108,7 +109,7 @@ while license_check(license_key):
                     time.sleep(60)
                     continue
                 # Sleep for an hour and check again.
-                time.sleep(5)
+                time.sleep(600)
                 continue
             # If today's date is 3 days prior to the full moon, iterate over token pairs and buy amounts, buy each
             # pair and wait three seconds in-between to avoid API lockout, sleep 24H after performing all buys from list
